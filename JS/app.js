93863d32abbc9944d38a3276d59df8dc18ae99b6
moneerzaki <moneerzaki@aucegypt.edu>
2022-09-07 01:54:16 +0200

    trying responsive nav bar

--- JS/app.js
+++ JS/app.js
@@ -3,27 +3,36 @@ const apiKey = "&APPID=85c0955723ea8d8ca16a480fd439457a&units=imperial";
 
 
 
-const submit = document.getElementById('submit');
-submit.addEventListener('click', performAction);
+// global variables.
+let city;
+let newDate;
+let description; 
+let degree; 
+let feeling; 
 
-let data1_city;
-let data2_desc; 
-let data3_degree; 
-let data4_feeling; 
-
+// defining elements in the web
 const data1  = document.querySelector('#data1'); // city name corresponding to the zip code
-const data2  = document.querySelector('#data2'); // weather description
-const data3  = document.querySelector('#data3'); // degrees in celisius and fehrenheit
-const data4  = document.querySelector('#data4'); // how are you feeling. 
+const data2  = document.querySelector('#data2'); // date
+const data3  = document.querySelector('#data3'); // weather description
+const data4  = document.querySelector('#data4'); // degrees in celisius and fehrenheit
+const data5  = document.querySelector('#data5'); // how are you feeling. 
+
 const userinputZIP =  document.getElementById('userinputZIP');
 const userinputFeel = document.getElementById('userinputFeel');
 
+const navMoreSymb = document.querySelector('.material-symbols-outlined');
+const navItems = document.querySelector('#nav-items');
+
+let d = new Date();
+newDate = d.getMonth()+1+'.'+ d.getDate()+'.'+ d.getFullYear();
 
 
 function performAction(e){
+
   const ZIPcode =  userinputZIP.value;
   const UserFeeling = userinputFeel.value;
   getWeather(baseURL,ZIPcode, apiKey);
+
 }
 
 const getWeather = async (baseURL, ZIPcode, key)=>{
@@ -33,20 +42,21 @@ const getWeather = async (baseURL, ZIPcode, key)=>{
 
     const data = await res.json();
     console.log(data);
-    data1_city = data.name; 
-    data2_desc = data.weather[0].description; 
-    data3_degree = data.main.temp;
-    // data4_feeling = UserFeeling;
+    city = data.name; 
+    description = data.weather[0].description; 
+    degree = data.main.temp;
+    // feeling = UserFeeling;
 
-    console.log(data1_city);
-    console.log(data2_desc);
-    console.log(data3_degree);
-    // console.log(data4_feeling);
+    console.log(city);
+    console.log(description);
+    console.log(degree);
+    console.log(feeling);
 
-    data1.innerHTML = `city: ${data1_city}`;
-    data2.innerHTML = `weather is: ${data2_desc}`;
-    data3.innerHTML = `degree: ${data3_degree}`;
-    data4.innerHTML = `User Feeling: ${data4_feeling}`;
+    data1.innerHTML = `city: ${city}`;
+    data2.innerHTML = `Date: ${newDate}`;
+    data3.innerHTML = `weather is: ${description}`;
+    data4.innerHTML = `degree: ${degree}`;
+    data5.innerHTML = `User Feeling: ${feeling}`;
     // console.log(data);
     return data;
   }  catch(error) {
@@ -57,3 +67,21 @@ const getWeather = async (baseURL, ZIPcode, key)=>{
 
 
 
+
+
+// Start the job
+const submit = document.getElementById('submit');
+submit.addEventListener('click', performAction);
+
+console.log("symb clicked");
+
+navMoreSymb.addEventListener('click', ()=>{
+  navItems.classList.toggle('nav-items-activate');
+  console.log("symb clicked");
+  // navItems.
+  console.log(navItems);
+  // navItems.style.backgroundColor = "red";
+  // navItems.getElementsByClassName.tranform(translateX('20vw'));
+  // navItems.style.transform = "translateX(0%)";
+  // myElement.style.transform = "translateX(" + (-200) + "px) scaleX(" + 1.6 + ") scaleY(" + 1.2 + ")";
+});
