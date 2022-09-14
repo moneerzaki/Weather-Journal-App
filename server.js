7440f855ecc0e403982cb43e9f9d6444e0b886b8
moneerzaki <moneerzaki@aucegypt.edu>
2022-09-14 22:26:22 +0200

    changing the file locations

--- server.js
+++ server.js
@@ -23,11 +23,20 @@ app.listen(port, ()=>{console.log(`connected to server at port ${port}`)});
 app.get('/', (req, res)=>{
     console.log("got successfully");
     console.log(Data);
+    // console.log(Data.body.city);
+    console.log(Data.city);
     res.send(Data);
 })
 
 app.post('/', (req, res)=>{
+    
     // Data.push(req.body);
-    Data = req.body;
+    Data['city'] = req.body.city;
+    Data['citnewDatey'] = req.body.newDate;
+    Data['description'] = req.body.description;
+    Data['degree'] = req.body.degree;
+    Data['feeling'] = req.body.feeling;
+    // console.log(Data);
     console.log("posted seccessfully");
+
 })
